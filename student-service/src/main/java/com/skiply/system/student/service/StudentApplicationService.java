3ed02f5fc77f30b554bd685ccc32c3d51fb1a540
wreckurring <wreckurring@gmail.com>
2026-07-14 01:15:29 +0530

    Improved student-service changes

--- student-service/src/main/java/com/skiply/system/student/service/StudentApplicationService.java
+++
@@ -1,9 +0,0 @@
-package com.skiply.system.student.service;
-
-import com.skiply.system.student.api.register.RegisterStudentCommand;
-import com.skiply.system.student.api.register.RegisterStudentResponse;
-
-public interface StudentApplicationService {
-
-    RegisterStudentResponse registerStudent(RegisterStudentCommand registerStudentCommand);
-}
